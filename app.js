1967d376ba53e8c39f3c1d3f91ad79cbd6b1776e
Marinela Rajkovic <marinela.rajkovic29@gmail.com>
2025-11-26 19:23:05 +0100

    Implement Firebase integration and update script type in HTML

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+const appSettings = {
+    databaseURL: "https://comida-e8cae-default-rtdb.europe-west1.firebasedatabase.app"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const foodInDB = ref(database, "food")
 
 const inputField = document.getElementById("input-field")
 const addButton = document.getElementById("add-button")
@@ -6,5 +15,6 @@ const addButton = document.getElementById("add-button")
 
 addButton.addEventListener("click", function() {
     let inputValue = inputField.value
+    push(foodInDB, inputValue)
     console.log(inputValue)
 })
