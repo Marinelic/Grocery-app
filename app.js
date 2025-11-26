c8cecbc502753d9a91aa2d44f6c0d434b7c74106
Marinela Rajkovic <marinela.rajkovic29@gmail.com>
2025-11-26 15:54:11 +0100

    Simple Mobile App for Grocery

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const inputField = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
+
+
+addButton.addEventListener("click", function() {
+    let inputValue = inputField.value
+    console.log(inputValue)
+})
